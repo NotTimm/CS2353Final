951131e9cd0c268eae72fb235c8ac8aa55f2b151
NotTimm <gamer.tdogtim@gmail.com>
2022-04-26 13:05:46 -0500

    Getting Started

---
+++ cmake-build-debug/CMakeFiles/2353Final.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "2353Final"
+  "2353Final.pdb"
+  "CMakeFiles/2353Final.dir/main.cpp.o"
+  "CMakeFiles/2353Final.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2353Final.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+std::vector<int> unionize(std::vector<int> left, std::vector<int> right)
+{
+
+}
+
+std::vector<int> intersect(std::vector<int> left, std::vector<int> right)
+{
+
+}
+
+std::vector<int> difference(std::vector<int> left, std::vector<int> toSubtract)
+{
+
+}
