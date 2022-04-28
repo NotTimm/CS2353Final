f341df895501475c2342dae18d67f7437ee311bb
katielaird <katielaird1224@gmail.com>
2022-04-28 17:00:00 -0500

    did parsing

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+using namespace std;
 
 std::vector<int> unionize(std::vector<int>& left, std::vector<int>& right)
 {
@@ -46,4 +46,35 @@ std::vector<int> difference(std::vector<int>& left, std::vector<int>& toSubtract
             out.push_back(i);
     }
     return out;
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    // reading in the file and adding to the two vectors
+    ifstream inFS;
+    inFS.open("../sets.txt");
+
+    if (!inFS.is_open()) { // make sure file opens
+        cout << "Could not open downhill.csv" << endl;
+        return 1;
+    }
+    string line;
+    getline(inFS, line);
+
+    while (getline(inFS, line)) {
+        vector<int> left;
+        vector<int> right;
+
+        // parse out each line and add to vector
+        stringstream lineSS(line); // create string stream
+        string set1Values, set2Values;
+
+        getline(lineSS, set1Values, ' ');
+        left.push_back(stod(set1Values));
+        getline(lineSS, set2Values, ' ');
+        right.push_back(stod(set2Values));
+
+        return 0;
+    }
 }
