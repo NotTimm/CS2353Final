48dabdcd04d48484091d0242fbb5ce9dd90edcff
NotTimm <gamer.tdogtim@gmail.com>
2022-05-01 20:21:16 -0500

    Program is working and I will record tommorow

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /mnt/c/Users/gamer/Desktop/2353Final
+# Install script for directory: /mnt/c/Users/Owner/Documents/DiscreteFinal
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/mnt/c/Users/gamer/Desktop/2353Final/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/mnt/c/Users/Owner/Documents/DiscreteFinal/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+using namespace std;
 
 std::vector<int> unionize(std::vector<int>& left, std::vector<int>& right)
 {
@@ -22,6 +22,7 @@ std::vector<int> unionize(std::vector<int>& left, std::vector<int>& right)
             out.push_back(i);
     }
     return out;
+
 }
 
 std::vector<int> intersect(std::vector<int>& left, std::vector<int>& right)
@@ -46,4 +47,46 @@ std::vector<int> difference(std::vector<int>& left, std::vector<int>& toSubtract
             out.push_back(i);
     }
     return out;
+}
+
+int main() {
+    // reading in the file and adding to the two vectors
+    ifstream inFS;
+    inFS.open("../sets.txt");
+
+    if (!inFS.is_open()) { // make sure file opens
+        cout << "Could not open set file" << endl;
+        return 1;
+    }
+    string set1, set2;
+    getline(inFS, set1);
+    getline(inFS, set2);
+    inFS.close();
+    vector<int> left;
+    vector<int> right;
+    // parse out each line and add to vector
+    stringstream lineSS(set1); // create string stream
+    stringstream lineSR(set2);
+    string cur;
+    while(lineSS >> cur)
+        left.push_back(stod(cur));
+    while(lineSR >> cur)
+        right.push_back(stod(cur));
+    vector<int> join, diff, inter;
+    join = unionize(left, right);
+    for (int i : join) {
+        cout << "Union Set: " << i << endl;
+    }
+
+    diff = difference(left, right);
+    for (int i : diff) {
+        cout << "Difference Set: " << i << endl;
+    }
+
+    inter = intersect(left, right);
+    for (int i : inter) {
+        cout << "Intersect Set: " << i << endl;
+    }
+
+    return 0;
 }
